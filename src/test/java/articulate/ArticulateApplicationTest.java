1b66a9f728ae56c538a4be27191798d4630fb2bb
hemantbarmecha <hemantbarmecha@gmail.com>
2018-08-14 16:01:09 -0500

    Update ArticulateApplicationTest.java

--- src/test/java/articulate/ArticulateApplicationTest.java
+++ src/test/java/articulate/ArticulateApplicationTest.java
@@ -1,5 +1,7 @@
 package articulate;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
