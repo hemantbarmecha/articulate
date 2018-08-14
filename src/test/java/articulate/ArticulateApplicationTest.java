4bcf5b9de73c1b4c57e2168f136cd454d48fa8d7
hemantbarmecha <hemantbarmecha@gmail.com>
2018-08-14 12:13:26 -0500

    Add files via upload

---
+++ src/test/java/articulate/ArticulateApplicationTest.java
@@ -0,0 +1,29 @@
+package articulate;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+public class ArticulateApplicationTest extends SpringBootArticulateTests{
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void test() throws Exception  {
+		//mockMvc.perform(get("/bluegreen-check")).andExpect(status().isOk());
+	}
+
+}
